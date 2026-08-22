621550ece78a932dd0dd55396f5518c3debdd796
Ner Karso <nerpkarso@gmail.com>
2026-08-22 18:34:28 -0300

    feat: integrate shadcn ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
@@ -6,4 +7,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [react()],
 });
